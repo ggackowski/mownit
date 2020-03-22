401aaf7c9fe007f8a7677459cab76d5f2dd87d16
Grzegorz Gackowski <greggupl1@gmail.com>
2020-03-22 13:22:52 +0100

    finished lab 1

--- lab1/4.cpp
+++ lab1/4.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <iomanip>
+#include <limits>
 
 int main() {
   float epsilon = 1.0f;
@@ -8,4 +9,7 @@ int main() {
   }
 
   std::cout << epsilon << std::endl;
+  std::cout << ((1.0f + epsilon) == 1) << std::endl;
+  std::cout << ((1.0f + epsilon * 0.5f) == 1) << std::endl;
+  
 }
---
+++ lab1/5popraw.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+  float a, b, c;
+  a = 0.1e1;
+  b = -0.2e11;
+  c = 0.2e01;
+  std::cout << a << " " << b << " " << c << std::endl;
+  float delta = b * b - 4 * a * c;
+  float res1 = (-b + sqrt(delta)) / (2 * a);
+  float res2 = c / (a * res1);
+  std::cout << res1 << " " << res2 << std::endl;
+}
+
